e4fccf7d5495a26c58cd03e444f452a11d294044
Hiroki Hayashi <hayashi.hiroki@mixense.jp>
2021-08-23 01:10:20 +0900

    ストップウォッチ作成

--- src/index.js
+++ src/index.js
@@ -1,17 +1,169 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+let playerList = [
+  'player 1',
+  'player 2',
+];
+
+class Timer extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      myTime: null,
+      intervalId : null,
+      time:{
+        second:0,
+        minute:0,
+        hour:0,
+      },
+      alert: false,
+      playerId:null,
+      limit: 30
+    };
+    this.handleSetLimit = this.handleSetLimit.bind(this);
+  }
+
+  handleStart() {
+    if(this.state.intervalId !== null){
+      return;
+    }
+    let intervalId = setInterval(() => {
+      this.update();
+    }, 1000);
+    this.setState({
+      startTime: new Date().getTime(),
+      nowTime: new Date().getTime(),
+      playerId: 0,
+      time:{
+        second:0,
+        minute:0,
+        hour:0,
+      },
+      alert: false,
+      intervalId: intervalId,
+    })
+  }
+
+  handleStop(){
+    clearInterval(this.state.intervalId);
+    this.setState({
+      intervalId: null
+    });
+  }
+
+  update(){
+    const now = new Date().getTime();
+    const start = this.state.startTime;
+    const elapsed = now - start;
+    let second = Math.floor(elapsed / 1000 % 60);
+    let minute = Math.floor(elapsed / 1000 / 60 % 60);
+    let hour = Math.floor(elapsed / 1000 / 60 / 60);
+    let alert = false;
+    if(elapsed >= this.state.limit * 1000){
+      alert = true;
+      clearInterval(this.state.intervalId);
+    }
+    this.setState({
+      time:{
+        second,
+        minute,
+        hour,
+      },
+      alert
+    })
+  }
+
+  handleChange(){
+    clearInterval(this.state.intervalId);
+
+    const nowPlayerId = this.state.playerId;
+    let nextPlayerId = (nowPlayerId + 1 >= playerList.length) ? 0 : nowPlayerId + 1;
+    let intervalId = setInterval(() => {
+      this.update();
+    }, 1000);
+
+    this.setState({
+      startTime: new Date().getTime(),
+      nowTime: new Date().getTime(),
+      time:{
+        second:0,
+        minute:0,
+        hour:0,
+      },
+      playerId: nextPlayerId,
+      intervalId: intervalId,
+      alert: false,
+    })
+  }
+
+  handleSetLimit(event){
+    this.setState({
+      limit: event.target.value
+    });
+  }
+
+
+  render(){
+    let className = this.state.alert ? 'alert' : '';
+    return (
+      <div>
+        <div>
+          {this.state.playerId !== null ? playerList[this.state.playerId] : '-'}
+        </div>
+        <div>
+          <button
+            className={className}
+            onClick={() => this.handleChange()}
+          >
+            {this.state.time.hour}:{this.state.time.minute}:{this.state.time.second}
+          </button>
+        </div>
+        <Buttons
+          onClickStart={() => this.handleStart()}
+          onClickStop={() => this.handleStop()}
+        />
+        <Setting
+          limit={this.state.limit}
+          onChangeLimit={this.handleSetLimit}
+        />
+      </div>
+    );
+  }
+}
+
+class Buttons extends React.Component{
+  render(){
+    return (
+      <div>
+        <button onClick={() => this.props.onClickStart()}>START</button>
+        <button onClick={() => this.props.onClickStop()}>Stop</button>
+      </div>
+    );
+  }
+}
+class Setting extends React.Component{
+  render(){
+    return (
+      <div>
+        <input name="limit" value={this.props.limit} onChange={this.props.onChangeLimit}/>
+      </div>
+    )
+  }
+}
+
+class Board extends React.Component {
+  render(){
+    return(
+      <div>
+        <Timer/>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Board/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
